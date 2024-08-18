cea1b436396bd3d5c6048c35ff1fc604b887e3fe
MeowVal <meowval@catkatnya.com>
2024-08-19 00:25:03 +0200

    VS setup test

---
+++ Source/DvZ/InventorySystem/DvZInventoryComponent.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DvZInventoryComponent.h"
+
+// Sets default values for this component's properties
+UDvZInventoryComponent::UDvZInventoryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDvZInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDvZInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
